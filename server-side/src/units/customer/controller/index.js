c5b0b464da3dde7a452de4ec7e9e740d6414cf88
Vinicius Azevedo <viniazvd@gmail.com>
2017-11-17 18:41:56 -0200

    v 0.0.22

--- server-side/src/units/customer/controller/index.js
+++ server-side/src/units/customer/controller/index.js
@@ -1,7 +1,7 @@
 const { services } = require('../../../domain').customers
 
 const [all, one, create, update, remove] = ['all', 'one', 'create', 'update', 'remove']
-  .map(actionName => require(`./${actionName}`))
-  .map(action => action(services))
+  .map(controller => require(`./${controller}`))
+  .map(inject => inject(services))
 
 module.exports = { all, one, create, update, remove }
