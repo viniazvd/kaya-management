1ea4a278d792950cc2fe28a907dec4e49f3deadd
viniazvd <viniazvd@gmail.com>
2017-12-02 14:44:52 -0200

    v 0.0.27

--- server-side/src/domain/customers/services/index.js
+++ server-side/src/domain/customers/services/index.js
@@ -3,30 +3,25 @@ const models = require('../../../infra/database/sequelize/models')
 let services = {}
 
 services.all = async () => {
-  const users = await models.user.findAll()
-  return users
+  return models.user.findAll()
 }
 
 services.one = async (id) => {
-  const user = models.user.findById(id)
-  return user
+  return models.user.findById(id)
 }
 
 services.create = async (name, email, password) => {
-  const newUser = models.user.build({ name, email, password })
+  const newUser = await models.user.build({ name, email, password })
 
-  const user = newUser.save()
-  return user
+  return newUser.save()
 }
 
 services.update = async (id, name, email, password) => {
-  const updateUser = models.user.update({ name, email, password }, { where: { id } })
-  return updateUser
+  return models.user.update({ name, email, password }, { where: { id } })
 }
 
 services.remove = async (id) => {
-  const deleteUser = models.user.destroy({ where: { id } })
-  return deleteUser
+  return models.user.destroy({ where: { id } })
 }
 
 module.exports = services
--- server-side/src/units/customer/controller/remove.js
+++ server-side/src/units/customer/controller/remove.js
@@ -2,6 +2,6 @@ module.exports = service => (req, res) => {
   const id = req.params.id
 
   service.remove(id)
-    .then(results => res.status(200).send(results))
+    .then(results => res.sendStatus(200))
     .catch(error => res.status(404).send(error))
 }
