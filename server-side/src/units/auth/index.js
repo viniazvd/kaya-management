0fb587763ff3486615cc82134eb62221d9656543
Vinicius Azevedo <viniazvd@gmail.com>
2017-12-23 12:44:25 -0200

    example of how to use catchErrors in comments

--- server-side/src/units/auth/index.js
+++ server-side/src/units/auth/index.js
@@ -1,7 +1,9 @@
 const { signup, signin, changePassword } = require('./middlewares')
+// const catchErrors = require('../../support/handleError')
 
 module.exports = server => {
   server.post('/api/auth/signup', signup)
+  // server.post('/api/auth/signup', catchErrors(signup))
   server.post('/api/auth/signin', signin)
   server.post('/api/auth/change-password', changePassword)
 }
