f8e071a6bddc412437dc3afb80bc68e40513bf1c
Vinicius Azevedo <viniazvd@gmail.com>
2017-12-27 16:39:55 -0200

    created a auth test file

---
+++ server-side/src/domain/auth/tests/auth.test.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+
+const isValid = require('../../../support/compare-passwords')
+const encrypt = require('../../../support/encrypt')
+const test = require('ava')
+const models = require('../../../infra/sequelize/models')
+
+test.beforeEach(t => models.user.destroy({truncate: true}))
+test.after.always(t => models.user.destroy({truncate: true}))
+
+test.serial('signup', async t => {
+  const user = { name: 'vinitest', email: 'emailtest@gmail.com', password: '123' }
+  const newUser = await models.user.build(user)
+  const result = await newUser.save()
+
+  t.is(result.dataValues.email, 'emailtest@gmail.com')
+})
+
+test.serial('signin', async t => {
+  const passwordEncrypted = await encrypt('123')
+  const user = { name: 'vinitest', email: 'emailtest@gmail.com', password: passwordEncrypted }
+  const newUser = await models.user.build(user)
+  const result = await newUser.save()
+
+  const isMatch = await isValid('123', user.password)
+
+  t.is(result.dataValues.email, 'emailtest@gmail.com')
+  t.is(isMatch, true)
+})
