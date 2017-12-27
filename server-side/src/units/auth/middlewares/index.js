223a23a1601ee02f298acb00cc2fd79ffbe85e2a
Vinicius Azevedo <viniazvd@gmail.com>
2017-12-27 02:33:05 -0200

    added forgotpassword in require map config

--- server-side/src/units/auth/middlewares/index.js
+++ server-side/src/units/auth/middlewares/index.js
@@ -1,7 +1,7 @@
 const { services } = require('../../../domain').auth
 
-const [signup, signin, changePassword] = ['signup', 'signin', 'changePassword']
+const [signup, signin, changePassword, forgotPassword] = ['signup', 'signin', 'changePassword', 'forgotPassword']
   .map(middlewares => require(`./${middlewares}`))
   .map(inject => inject(services))
 
-module.exports = { signup, signin, changePassword }
+module.exports = { signup, signin, changePassword, forgotPassword }
