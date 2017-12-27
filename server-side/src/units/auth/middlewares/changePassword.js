a3163ac54cc749495a7eab94da1a499e621c8e3b
Vinicius Azevedo <viniazvd@gmail.com>
2017-12-27 02:32:21 -0200

    added token param

--- server-side/src/units/auth/middlewares/changePassword.js
+++ server-side/src/units/auth/middlewares/changePassword.js
@@ -1,7 +1,7 @@
 module.exports = service => (req, res) => {
-  const { email, password, newPassword } = req.body
+  const { token, email, newPassword } = req.body
 
-  service.changePassword(email, password, newPassword)
+  service.changePassword(token, email, newPassword)
     .then(result => res.status(200).send({ result }))
     .catch(error => res.status(422).send({ error: error.message }))
 }
