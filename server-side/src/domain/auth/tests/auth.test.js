6be02cc0832c13a3f9f97314c1e316449d93051f
Vinicius Azevedo <viniazvd@gmail.com>
2017-12-28 13:11:59 -0200

    updating model.user to model.Client in tests

--- server-side/src/domain/auth/tests/auth.test.js
+++ server-side/src/domain/auth/tests/auth.test.js
@@ -5,12 +5,12 @@ const encrypt = require('../../../support/encrypt')
 const test = require('ava')
 const models = require('../../../infra/sequelize/models')
 
-test.beforeEach(t => models.user.destroy({truncate: true}))
-test.after.always(t => models.user.destroy({truncate: true}))
+test.beforeEach(t => models.Client.destroy({truncate: true}))
+test.after.always(t => models.Client.destroy({truncate: true}))
 
 test.serial('signup', async t => {
   const user = { name: 'vinitest', email: 'emailtest@gmail.com', password: '123' }
-  const newUser = await models.user.build(user)
+  const newUser = await models.Client.build(user)
   const result = await newUser.save()
 
   t.is(result.dataValues.email, 'emailtest@gmail.com')
@@ -19,7 +19,7 @@ test.serial('signup', async t => {
 test.serial('signin', async t => {
   const passwordEncrypted = await encrypt('123')
   const user = { name: 'vinitest', email: 'emailtest@gmail.com', password: passwordEncrypted }
-  const newUser = await models.user.build(user)
+  const newUser = await models.Client.build(user)
   const result = await newUser.save()
 
   const isMatch = await isValid('123', user.password)
