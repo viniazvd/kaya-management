8a0088f363811bdd25d97ba48e293ca7f36fee7c
Vinicius Azevedo <viniazvd@gmail.com>
2017-12-28 15:14:05 -0200

    refactoring

--- server-side/src/support/middlewares/jwt/index.js
+++ server-side/src/support/middlewares/jwt/index.js
@@ -6,7 +6,7 @@ module.exports = (deps) => {
       const token = req.headers['x-access-token']
 
       if (!token) {
-        res.send(403, { error: 'Token não fornecido' })
+        res.send(403).send({ error: 'Token não fornecido' })
         return false
       }
 
