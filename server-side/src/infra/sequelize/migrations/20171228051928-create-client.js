61a22065f1d9bce08176989c7556e9cabdc074d6
Vinicius Azevedo <viniazvd@gmail.com>
2017-12-28 13:12:19 -0200

    removing unique propertie in email

--- server-side/src/infra/sequelize/migrations/20171228051928-create-client.js
+++ server-side/src/infra/sequelize/migrations/20171228051928-create-client.js
@@ -11,8 +11,7 @@ module.exports = {
         type: Sequelize.STRING
       },
       email: {
-        type: Sequelize.STRING,
-        unique: true
+        type: Sequelize.STRING
       },
       password: {
         type: Sequelize.STRING
