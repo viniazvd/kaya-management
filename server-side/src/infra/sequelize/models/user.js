631b04fefd8a42ce89aac5b9bb768a70752aeeb7
Vinicius Azevedo <viniazvd@gmail.com>
2017-12-28 12:13:17 -0200

    deleted

--- server-side/src/infra/sequelize/models/user.js
+++
@@ -1,9 +0,0 @@
-module.exports = (sequelize, DataTypes) => {
-  const user = sequelize.define('user', {
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
-  })
-
-  return user
-}
