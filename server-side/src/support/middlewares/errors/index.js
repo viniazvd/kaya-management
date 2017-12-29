8a382ce62319084b3801f5b40c4d963586059f0f
Vinicius Azevedo <viniazvd@gmail.com>
2017-12-28 23:12:38 -0200

    use only one handler (logErrors)

--- server-side/src/support/middlewares/errors/index.js
+++ server-side/src/support/middlewares/errors/index.js
@@ -1,6 +1,4 @@
 // const catchErrors = require('./catchErrors')
-const clientErrorHandler = require('./clientErrorHandler')
-const errorHandler = require('./errorHandler')
 const logErrors = require('./logErrors')
 
-module.exports = { clientErrorHandler, errorHandler, logErrors }
+module.exports = { logErrors }
