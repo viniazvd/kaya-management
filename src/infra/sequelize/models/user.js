b3489dbd1d05b6fb14e240588c3891b16a2c2edb
Vinicius Azevedo <viniazvd@gmail.com>
2018-01-09 15:12:33 -0200

    add defaultValue

--- src/infra/sequelize/models/user.js
+++ src/infra/sequelize/models/user.js
@@ -3,7 +3,7 @@ module.exports = (sequelize, DataTypes) => {
     name: DataTypes.STRING,
     email: { type: DataTypes.STRING, allowNull: false, unique: true },
     password: { type: DataTypes.STRING, allowNull: false },
-    role: { type: DataTypes.STRING, allowNull: false }
+    role: { type: DataTypes.STRING, allowNull: true, defaultValue: 'USER' }
   })
 
   // User.associate = models => User.hasMany(models.Task)
